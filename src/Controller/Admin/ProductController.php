53c94b2bd298a0bd70cd3b197cca628df66a276e
ArtemSykhetskiy <2210ams@gmail.com>
2022-10-24 17:32:43 +0300

    refactoring

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use App\Form\ProductFormType;
+use App\Form\ProfileEditFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,9 +12,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route ("/admin")
+ */
 class ProductController extends AbstractController
 {
-    #[Route('/admin/products', methods: 'get', name: 'admin.products')]
+    #[Route('/products', methods: 'get', name: 'admin.products')]
     public function index(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
@@ -22,7 +27,7 @@ class ProductController extends AbstractController
         return $this->render('product/index.html.twig', ['products' => $products]);
     }
 
-    #[Route('/admin/products/add', methods: 'get', name: 'admin.add.products')]
+    #[Route('/products/add', methods: 'get', name: 'admin.add.products')]
     public function create(): Response
     {
         $form = $this->createForm(ProductFormType::class);
@@ -30,7 +35,7 @@ class ProductController extends AbstractController
         return $this->render('product/add.html.twig', ['form' => $form->createView()]);
     }
 
-    #[Route('/admin/products/store', methods: 'post', name: 'admin.store.products')]
+    #[Route('/products/store', methods: 'post', name: 'admin.store.products')]
     public function store(ManagerRegistry $doctrine)
     {
         $entityManager = $doctrine->getManager();
@@ -48,30 +53,30 @@ class ProductController extends AbstractController
         return $this->redirectToRoute('admin.products');
     }
 
-    #[Route('/admin/products/edit/{id}', methods: 'get', name: 'admin.edit.products')]
-    public function edit(int $id, ManagerRegistry $doctrine): Response
+    #[Route('/products/edit/{id}', name: 'admin.edit.products')]
+    public function edit(Request $request, Product $product, ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
-        $product = $entityManager->getRepository(Product::class)->find($id);
-        $form = $this->createForm(ProductFormType::class);
-        $form->getData($product);
+        $form = $this->createForm(ProductFormType::class, $product);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin.products');
+        }
 
         return $this->render('product/edit.html.twig', ['form' => $form->createView(), 'product' => $product]);
     }
 
-    #[Route('/admin/products/update/{id}', methods: 'post', name: 'admin.update.products')]
-    public function update(Request $request, int $id, ManagerRegistry $doctrine): Response
+    #[Route('/products/remove/{id}', name: 'admin.remove.products')]
+    public function remove(Product $product, ManagerRegistry $doctrine)
     {
         $entityManager = $doctrine->getManager();
-        $product = $entityManager->getRepository(Product::class)->find($id);
-
-        $product->setTitle($_POST['product_form']['Title']);
-        $product->setDescription($_POST['product_form']['Description']);
-        $product->setSKU($_POST['product_form']['SKU']);
-        $product->setPrice($_POST['product_form']['Price']);
-
-        $entityManager->merge($product);
-
+        $entityManager->remove($product);
         $entityManager->flush();
 
         return $this->redirectToRoute('admin.products');
